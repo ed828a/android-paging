ec5b5a46c91e29ca5a379f1ea7738061ce3cc704
Edward <ed828a@gmail.com>
2018-06-04 07:45:31 +0930

    take out some unuseful comments

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -16,11 +16,9 @@
 
 package com.example.android.codelabs.paging.data
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.paging.LivePagedListBuilder
 import android.util.Log
 import com.example.android.codelabs.paging.api.GithubService
-import com.example.android.codelabs.paging.api.searchRepos
 import com.example.android.codelabs.paging.db.GithubLocalCache
 import com.example.android.codelabs.paging.model.RepoSearchResult
 
@@ -31,23 +29,11 @@ class GithubRepository(
         private val service: GithubService,
         private val cache: GithubLocalCache
 ) {
-
-    // keep the last requested page. When the request is successful, increment the page number.
-//    private var lastRequestedPage = 1
-
-    // LiveData of network errors.
-//    private val networkErrors = MutableLiveData<String>()
-
-    // avoid triggering multiple requests in the same time
-//    private var isRequestInProgress = false
-
     /**
      * Search repositories whose names match the query.
      */
     fun search(query: String): RepoSearchResult {
         Log.d("GithubRepository", "New query: $query")
-//        lastRequestedPage = 1
-//        requestAndSaveData(query)
 
         // Get data from the local cache
         val dataSourceFactory = cache.reposByName(query)
@@ -61,26 +47,6 @@ class GithubRepository(
 
         return RepoSearchResult(data, networkErrors)
     }
-
-//    fun requestMore(query: String) {
-//
-//        requestAndSaveData(query)
-//    }
-
-//    private fun requestAndSaveData(query: String) {
-//        if (isRequestInProgress) return
-//
-//        isRequestInProgress = true
-//        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
-//            cache.insert(repos, {
-//                lastRequestedPage++
-//                isRequestInProgress = false
-//            })
-//        }, { error ->
-//            networkErrors.postValue(error)
-//            isRequestInProgress = false
-//        })
-//    }
 
     companion object {
         //        private const val NETWORK_PAGE_SIZE = 50
